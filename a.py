0acb78387355c553a76e58d99f446263d0ed20f8
guiisc <62407390+guiisc@users.noreply.github.com>
2021-09-07 13:35:44 -0300

    Update a.py

--- a.py
+++ a.py
@@ -13,7 +13,7 @@ times = st.radio('Numero de times', [2, 3, 4])
 jog_port_time = st.radio('Jogadores por time', [4, 5, 6, 7])
 
 def times_f(jogadores_hoje, times, jog_port_time):
-    columns = [ f'time {i+1}' for i in range(len(times)+1) ]
+    columns = [ f'time {i+1}' for i in range(times+1) ]
     out = pd.DataFrame(columns=columns, index=np.arange(1, jog_port_time+1))
     jog_sorteados = []
     while len(jogadores_hoje) > 0:
